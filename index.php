5725cf581d4020ce4ec042505b4140c8f5ccda2d
hanzi99@126.com <github7758521>
2014-09-18 22:50:28 +0800

    test

--- index.php
+++ index.php
@@ -19,8 +19,6 @@ define('APP_DEBUG',True);
 
 // 定义应用目录
 define('APP_PATH','./Apps/');
-$file = APP_PATH."Home/index.html";
-file_put_contents($file,"1234567890");
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
 
