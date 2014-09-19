62e7d02a8a55c77f012cd40057064e3d22aa2e06
hanziacier <hanzi99@126.com>
2014-09-19 11:52:12 +0800

    test

---
+++ Apps/Common/Model/UserModel.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leju
+ * Date: 14-9-19
+ * Time: 上午10:48
+ */
+
+namespace Common\Model;
+
+use Think\Model;
+class UserModel extends Model {
+    protected $tablePrefix = '';
+} 
