b03d58ea8a0a523b7fac8dd4a1777bfe842e4cc6
hanziacier <hanzi99@126.com>
2014-09-19 15:05:31 +0800

    test

--- Apps/Common/Conf/config.php
+++ Apps/Common/Conf/config.php
@@ -3,7 +3,7 @@
 return  array(
     /* 数据库设置 */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  $_SERVER['MYSQLDB_HOST'].','.$_SERVER['MYSQLDB_HOST'], // 服务器地址
+    'DB_HOST'               =>  $_SERVER['MYSQLDB_HOST'].','.$_SERVER['MYSQLDB_HOST'], // 数据库服务器地址
     'DB_NAME'               =>  $_SERVER['MYSQLDB_NAME'],          // 数据库名
     'DB_USER'               =>  $_SERVER['MYSQLDB_USER'],      // 用户名
     'DB_PWD'                =>  $_SERVER['MYSQLDB_PASSWORD'],          // 密码
