a0a263f5504b0ef06380c4c2c268836cdbcbb981
hanzi99@126.com <github7758521>
2014-09-22 23:13:42 +0800

    test

--- Apps/Home/Controller/IndexController.class.php
+++ Apps/Home/Controller/IndexController.class.php
@@ -4,9 +4,11 @@ use Think\Controller;
 class IndexController extends Controller {
 
     public function index(){
-        $user = new \Common\Model\UserModel("User");
+        //$user = new \Common\Model\UserModel("User");
+        $user = D("User");
         var_dump($user->getUser());
-        $login = new \Common\Logic\LoginLogic("Login");
+       // $login = new \Common\Logic\LoginLogic("Login");
+        $login = D("Login","Logic");
         var_dump($login->login());
         $this->display();
     }
