79ccef6d875fae5248d123994deed0fd0d66903f
hanziacier <hanzi99@126.com>
2014-10-08 17:23:39 +0800

    qq

--- Apps/Home/Controller/IndexController.class.php
+++ Apps/Home/Controller/IndexController.class.php
@@ -6,10 +6,10 @@ class IndexController extends Controller {
     public function index(){
         //$user = new \Common\Model\UserModel("User");
         $user = D("User");
-        var_dump($user->getUser());
-       // $login = new \Common\Logic\LoginLogic("Login");
-        $login = D("Login","Logic");
-        $login->login();
+        //var_dump($user->getUser());
+        // $login = new \Common\Logic\LoginLogic("Login");
+        //$login = D("Login","Logic");
+        //$login->login();
         $this->display();
     }
 }
