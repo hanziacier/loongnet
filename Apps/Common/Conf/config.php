af0b365c8ef9ff8664d5b60132207e80a9735e9f
hanziacier <hanzi99@126.com>
2014-10-09 15:05:15 +0800

    qq

--- Apps/Common/Conf/config.php
+++ Apps/Common/Conf/config.php
@@ -9,7 +9,12 @@ return  array(
     'DB_PWD'                =>  $_SERVER['MYSQLDB_PASSWORD'],          // 密码
     'DB_PORT'               =>  $_SERVER['MYSQLDB_PORT'],        // 端口
     'DB_PREFIX'             =>  '',    // 数据库表前缀
+    'DB_CHARSET' => 'utf8', // 数据库编码默认采用utf8
+    'DB_DEPLOY_TYPE' => 1, // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
+    'DB_RW_SEPARATE' => true, // 数据库读写是否分离 主从式有效
+    'DB_MASTER_NUM' => 1, // 读写分离后 主服务器数量
 
     'LOAD_EXT_CONFIG' => 'alias', // 加载扩展配置文件
+
 
 );
